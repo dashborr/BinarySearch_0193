4805e3e6d204e11da98adb213255b34eb908d55d
dashborr <danisarfas7@gmail.com>
2026-04-17 15:46:34 +0700

    Menambahkan algoritma bubble sort untuk mengurutkan data array secara ascending

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -27,3 +27,20 @@ void input()
 }
 
 void BubbleSortArray()
+{
+    int pass = 1;
+
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
