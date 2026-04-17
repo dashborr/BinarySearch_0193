92041cb9ec22ea39325b2e6e7849499a1515608e
dashborr <danisarfas7@gmail.com>
2026-04-17 15:43:46 +0700

    menambahkan jumlah elemen array maksimal 10 pada fungsi input()

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -6,3 +6,16 @@ int nPanjang;
 int x;
 
 void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+            break;
+        else
+            cout << "[!] Jumlah elemen tidak boleh lebih dari 10!\n";
+    }
+    
+}
